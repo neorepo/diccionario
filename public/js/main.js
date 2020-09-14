5585d218858820237546438debe641cb8b95bfca
neo <neorepo@gmail.com>
2020-09-13 21:32:45 -0300

    Realizando cambios finales

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,34 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-
     // initDeleteFlash();
+    initErrorFields();
     initFlashes();
     initDeleteDialog();
+    initPreventFormSubmit();
 });
-
-//Get the button
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
 
 function initDeleteFlash() {
     if (document.querySelector('.alert')) {
         document.querySelectorAll('.alert').forEach(el => {
-            fadeOut(el);
+            setTimeout(function () { el.remove(); }, 3000);
         });
     }
 }
 
-function fadeOut(el) {
-    setTimeout(function () { /*el.style.display = 'none';*/ el.remove(); }, 3000);
+function initPreventFormSubmit() {
+    // Evitar enviar el formulario presionando la tecla ENTER en un input field
+    if (document.querySelector('form')) {
+        // También se puede utilizar el evento onkeydown
+        document.querySelector('form').onkeypress = (e) => {
+            if (e.key === "Enter") {
+                // Evitamos que se ejecuté el evento
+                e.preventDefault();
+                // Retornamos false
+                return false;
+            }
+        }
+    }
 }
 
 function initFlashes() {
@@ -72,3 +64,32 @@ function initDeleteDialog() {
         return false;
     });
 }
+
+function initErrorFields() {
+    // Captura el primer error que exista en los formularios de registro
+    if (document.querySelector('.is-invalid')) {
+        document.querySelector('.is-invalid').focus();
+    }
+}
+
+/* Lógica volver a arriba */
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+/* Fin de la lógica de volver a arriba*/
